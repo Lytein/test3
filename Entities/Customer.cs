a82828ae3c2eef51e87f93b3a26cb207eba00908
Lytein <119023898+Lytein@users.noreply.github.com>
2025-12-15 02:55:04 +0700

    Add files via upload

---
+++ Entities/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StoreManagementBlazorApp.Entities
+{
+    public class Customer
+    {
+        public int customer_id { get; set; }
+        public string? name { get; set; }
+        public string? phone { get; set; }
+        public string? email { get; set; }
+        public string? address { get; set; }
+        public DateTime created_at { get; set; }
+    }
+}
---
+++ Entities/Product.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StoreManagementBlazorApp.Entities
+{
+    [Table("Products")]
+    public class Product
+    {
+        [Key]
+        public string Id { get; set; } = "";
+
+        [Required, MaxLength(100)]
+        public string Name { get; set; } = "";
+
+        [MaxLength(500)]
+        public string Description { get; set; } = "";
+
+        [MaxLength(50)]
+        public string Category { get; set; } = "";
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+
+        public int Stock { get; set; }
+
+        [MaxLength(200)]
+        public string Image { get; set; } = "";
+    }
+}
